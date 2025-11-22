3a4c1a52b59a2027113b372412f884f634148a3e
4trastos <info@4trastos.com>
2025-11-22 19:33:30 +0100

    safe path

--- src/show_options.c
+++ src/show_options.c
@@ -18,11 +18,18 @@ void    show_version(void)
 void    show_help(void)
 {
     printf("Usage:\n");
-    printf("./ft_traceroute [ -m max_ttl ]\n");
+    printf("./ft_traceroute [ -i device ] [ -m max_ttl ] [ -t tos ] [ -q nqueries ]\n");
     printf("Options:\n");
+    printf("-i device  --interface=device\n");
+    printf("                            Specify a network interface to operate with\n");
     printf("-m max_ttl  --max-hops=max_ttl\n");
-    printf("                              Set the max number of hops (max TTL to be\n");
-    printf("                              reached). Default is 30\n");
+    printf("                            Set the max number of hops (max TTL to be\n");
+    printf("                            reached). Default is 30\n");
+    printf("-t tos  --tos=tos           Set the TOS (IPv4 type of service) or TC (IPv6\n");
+    printf("                            traffic class) value for outgoing packets\n");
+    printf("-q nqueries  --queries=nqueries\n");
+    printf("                            Set the number of probes per each hop. Default is\n");
+    printf("                            3\n");
     printf("-V  --version               Print version info and exit\n");
     printf("--help                      Read this help and exit\n");
     printf("\nArguments:\n");
